e1e909691fd576f7f7f56cb3d71928a33e0e8abf
WJGriffin9262 <william9262@gmail.com>
2025-10-08 12:51:27 -0500

    modal still not working troubleshoot

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ function contact(event) {
 }
 let isModalOpen = false;
 function toggleModal() {
-    if(isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open");
-    }
-    isModalOpen = true;
-    document.body.classList += "modal--open";
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
